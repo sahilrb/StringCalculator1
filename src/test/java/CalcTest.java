c316a2808dd84f7c16a4756ab917a62b601bb53c
Sahil Bhingradiya <sahilbhingradia28@gmail.com>
2021-08-29 18:43:08 +0530

    4. Delimiters Error

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -5,38 +5,50 @@ import org.junit.Assert;
 public class CalcTest {
     Calc calc;
 
-    @Before
-    public void init() {
-        calc = new Calc();
-    }
-
-    @Test
-    public void isEmptyCheck() {
-        public void isEmpty() {
-            Assert.assertEquals(0,calc.Add(""));
+        @Before
+        public void init() {
+            calc = new Calc();
         }
 
-    @Test
-    public void isSingleValue() {
-    public void isSingle() {
-        Assert.assertEquals(1,calc.Add("1"));
-    }
+        @Test
+        public void isEmptyCheck() {
+            public void isEmpty() {
+                Assert.assertEquals(0,calc.Add(""));
+            }
 
-    @Test
-    public void sumofTwoValue() {
-        public void sumofTwo() {
-            Assert.assertEquals(3,calc.Add("1,2"));
+        @Test
+        public void isSingleValue() {
+        public void isSingle() {
+            Assert.assertEquals(1,calc.Add("1"));
         }
 
-    @Test
-    public void sumofMultipleValue() {
-        public void sumofMultiple() {
-            Assert.assertEquals(15,calc.Add("1,2,3,4,5"));
+        @Test
+        public void sumofTwoValue() {
+            public void sumofTwo() {
+                Assert.assertEquals(3,calc.Add("1,2"));
+            }
+
+        @Test
+        public void sumofMultipleValue() {
+            public void sumofMultiple() {
+                Assert.assertEquals(15,calc.Add("1,2,3,4,5"));
+            }
+
+        @Test
+        public void sumofMultipleNewline() {
+                Assert.assertEquals(21,calc.Add("1,2\n3\n4,5,6"));
+            }
+        @Test
+        public void onlyNewline() {
+            Assert.assertEquals(0,calc.Add("\n\n\n\n\n\n"));
         }
 
-    @Test
-    public void sumofMultipleNewline() {
-            Assert.assertEquals(15,calc.Add("1,2,3\n4,5"));
+        @Test
+        public void delimiters() {
+            Assert.assertEquals(6,calc.Add("//;\n\n1;2;3"));
         }
+
+
+
     }
 }
